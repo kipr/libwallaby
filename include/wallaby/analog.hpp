1f121430909aed4779b9ea4b8385b2405692925b
Joshua Southerland <js@ou.edu>
2015-11-03 17:04:05 -0600

    Adding public analog C/C++ headers

---
+++ include/wallaby/analog.hpp
@@ -0,0 +1,37 @@
+/*
+ * analog.hpp
+ *
+ *  Created on: Nov 3, 2015
+ *      Author: Joshua Southerland
+ */
+
+#ifndef INCLUDE_WALLABY_ANALOG_HPP_
+#define INCLUDE_WALLABY_ANALOG_HPP_
+
+class Analog
+{
+public:
+	Analog(unsigned char port);
+	virtual void ~Analog();
+
+	virtual unsigned short value() const;
+
+	void setPullup(bool pullup);
+	bool pullup() const;
+
+	unsigned char port() const;
+private:
+	unsigned char m_port;
+};
+
+class Analog8 : public Analog
+{
+public:
+	Analog8(unsigned char port);
+	virtual void ~Analog8();
+
+	unsigned short value() const;
+};
+
+
+#endif /* INCLUDE_WALLABY_ANALOG_HPP_ */
