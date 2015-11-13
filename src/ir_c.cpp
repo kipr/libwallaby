fc419cba123e5ac5e0a5bee2b3fc16bb245cd236
Joshua Southerland <js@ou.edu>
2015-11-13 16:29:29 -0600

    Adding ir functions for backwards compatibility

---
+++ src/ir_c.cpp
@@ -0,0 +1,20 @@
+/*
+ * ir_c.cpp
+ *
+ *  Created on: Nov 13, 2015
+ *      Author: Joshua Southerland
+ */
+
+
+#include "wallaby/ir.h"
+#include "wallaby/ir.hpp"
+
+void ir_read()
+{
+	Ir::read();
+}
+
+void ir_write()
+{
+	Ir::write();
+}
