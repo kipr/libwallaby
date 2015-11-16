6b4e04048fe4a350451733a938e4bea7ccb923b5
Joshua Southerland <js@ou.edu>
2015-11-16 16:33:20 -0600

    Monitoring distance and angle with the create test

--- tests/create_test.c
+++ tests/create_test.c
@@ -12,6 +12,8 @@
 
 int main(int argc, char ** argv)
 {
+	int i;
+
 	printf("Connecting...\n");
 	create_connect();
 
@@ -20,8 +22,23 @@ int main(int argc, char ** argv)
 	printf("Spinning CW...n");
 	create_spin_CW(40);
 
-	// spin a while
-	msleep(10000);
+	// spin a while CW
+	for (i = 0; i < 10; ++i)
+	{
+		printf("Distance: %d  Angle: %d\n", get_create_distance(), get_create_total_angle());
+		msleep(1000);
+	}
+
+	printf("Spinning CCW...n");
+	create_spin_CCW(40);
+
+	// spin a while CCW
+	for (i = 0; i < 10; ++i)
+	{
+		printf("Distance: %d  Angle: %d\n", get_create_distance(), get_create_total_angle());
+		msleep(1000);
+	}
+
 
 	printf("Stopping...\n");
 	create_stop();
