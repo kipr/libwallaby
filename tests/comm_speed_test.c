327b1bca5c41211346fabebea8aa267b35c6ba87
Joshua Southerland <js@ou.edu>
2016-01-02 15:54:59 -0600

    Updating SPI communication speed test to make use of the new transfer update count in Battlehill

--- tests/comm_speed_test.c
+++ tests/comm_speed_test.c
@@ -19,26 +19,22 @@ long elapsed_usec(struct timeval t1, struct timeval t2)
 
 int main(int argc, char ** argv)
 {
-	set_digital_output(12, 1);
-
     while(1)
     {
     	struct timeval start, stop;
-    	static const unsigned int num_samples = 100;
+    	unsigned long int samples_start, samples_stop, num_samples;
 
     	gettimeofday(&start, NULL);
 
-    	unsigned int i;
-    	for (i = 0; i < num_samples; ++i)
-    	{
-    		float v = power_level();
-        	if (v < 6.0 || v > 7.5)
-			{
-        		printf("%f\n",v);
-			}
-    	}
+    	samples_start = get_robot_update_count();
+
+    	usleep(100000);
+
+    	samples_stop = get_robot_update_count();
 
     	gettimeofday(&stop, NULL);
+
+    	num_samples = samples_stop - samples_start;
 
     	long elapsed_time = elapsed_usec(start, stop);
     	double avg_sample_time_usec = ((double)elapsed_time)/((double)num_samples);
