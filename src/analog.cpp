c02770a2c23d997c100c9f6bec8ace64c172fd3e
Joshua Southerland <js@ou.edu>
2016-01-06 14:31:25 -0600

    Switching to building a shared lib

--- src/analog.cpp
+++ src/analog.cpp
@@ -16,6 +16,11 @@ Analog::Analog(unsigned char port)
 
 }
 
+Analog::~Analog()
+{
+
+}
+
 unsigned short Analog::value() const
 {
 	return Private::BattleHill::instance()->getAnalogValue(m_port);
@@ -43,9 +48,19 @@ unsigned short Analog8::value() const
 	return Analog::value() >> 4;
 }
 
+Analog8::~Analog8()
+{
+
+}
+
 
 Analog10::Analog10(unsigned char port)
 : Analog(port)
+{
+
+}
+
+Analog10::~Analog10()
 {
 
 }
