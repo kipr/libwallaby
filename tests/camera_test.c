ba38f0c630c61389a1d6f5666297604f4f52da1b
Nafis Zaman <nafis.zaman-1@ou.edu>
2016-01-07 12:49:15 +0600

    Adding simple camera test

---
+++ tests/camera_test.c
@@ -0,0 +1,55 @@
+/*
+ * camera_test.c
+ *
+ *  Created on: Jan 7, 2015
+ *      Author: Nafis Zaman
+ */
+
+#include <stdio.h>
+
+#include "wallaby/wallaby.h"
+#include "wallaby/camera.h"
+#include "wallaby/geom.h"
+
+int main(int argc, char ** argv)
+{
+  printf("Opening camera... ");
+  int ret = camera_open_at_res(MED_RES);
+  printf("Success? %d\n", ret);
+  printf("\n");
+  
+  printf("Waiting 3 seconds...\n");
+  msleep(3000L);
+  
+  printf("Camera width: %d\n", get_camera_width());
+  printf("Camera height: %d\n", get_camera_height());
+  printf("\n");
+  
+  for(int i = 1; i <= 10; ++i) {
+    printf("SAMPLE %d\n", i);
+    printf("Updating camera... ");
+    ret = camera_update();
+    printf("Success? %d\n", ret);
+    
+    const int count = get_object_count(0);
+    const int conf = get_object_confidence(0, 0);
+    const int area = get_object_area(0, 0);
+    const int x = get_object_centroid_x(0, 0);
+    const int y = get_object_centroid_y(0, 0);
+    
+    printf("Object count: %d\n", count);
+    printf("Largest object confidence: %d\n", conf);
+    printf("Largest object area: %d\n", area);
+    printf("Largest object center: (%d, %d)\n", x, y);
+    printf("\n");
+    
+    msleep(1000L);
+  }
+  
+  printf("Closing camera...\n");
+  camera_close();
+  
+  printf("Done!\n");
+  
+  return 0;
+}
