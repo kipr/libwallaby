2e1d7bd2d14d4c2c4a9d9b80dfe5a57d35bb97f0
Nafis Zaman <nafis.zaman-1@ou.edu>
2016-01-09 01:57:36 +0600

    Modified cam test for debugging

--- tests/camera_test.c
+++ tests/camera_test.c
@@ -19,18 +19,28 @@ int main(int argc, char ** argv)
   printf("Waiting 3 seconds...\n");
   msleep(3000L);
   
+  /*printf("Loading test1...\n");
+  camera_load_config("test1");
+  msleep(2000L);
+  printf("Loaded\n");
+  
+  printf("Loading test1...\n");
+  camera_load_config("test2");
+  msleep(2000L);
+  printf("Loaded\n");*/
+  
   printf("Camera width: %d\n", get_camera_width());
   printf("Camera height: %d\n", get_camera_height());
   printf("\n");
   
   int i;
-  for(i = 1; i <= 10; ++i) {
-    printf("SAMPLE %d\n", i);
-    printf("Updating camera... ");
+  for(i = 1; i <= 100; ++i) {
+    //printf("SAMPLE %d\n", i);
+    //printf("Updating camera... ");
     ret = camera_update();
-    printf("Success? %d\n", ret);
+    //printf("Success? %d\n", ret);
     
-    const int count = get_object_count(0);
+    /*const int count = get_object_count(0);
     const int conf = get_object_confidence(0, 0);
     const int area = get_object_area(0, 0);
     const int x = get_object_centroid_x(0, 0);
@@ -42,7 +52,7 @@ int main(int argc, char ** argv)
       printf("Largest object area: %d\n", area);
       printf("Largest object center: (%d, %d)\n", x, y);
     }
-    printf("\n");
+    printf("\n");*/
     
     msleep(1000L);
   }
