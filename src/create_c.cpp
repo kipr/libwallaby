4861144a0b37cd107ece60b7c76e35e8f9c9e346
Braden McDorman <bmcdorman@gmail.com>
2016-01-08 21:10:05 -0600

    add function defns

--- src/create_c.cpp
+++ src/create_c.cpp
@@ -157,6 +157,40 @@ int get_create_rclightbump()
   return Create::instance()->lightBumpCenterRight()->value() ? 1 : 0;
 }
 
+int vi_get_create_llightbump_amt()
+{
+  return Create::instance()->lightBumpLeftSignal()->value();
+}
+
+
+int vi_get_create_lflightbump_amt()
+{
+  return Create::instance()->lightBumpFrontLeftSignal()->value();
+}
+
+
+int vi_get_create_lclightbump_amt()
+{
+  return Create::instance()->lightBumpCenterLeftSignal()->value();
+}
+
+int vi_get_create_rlightbump_amt()
+{
+  return Create::instance()->lightBumpRightSignal()->value();
+}
+
+
+int vi_get_create_rflightbump_amt()
+{
+  return Create::instance()->lightBumpFrontRightSignal()->value();
+}
+
+
+int vi_get_create_rclightbump_amt()
+{
+  return Create::instance()->lightBumpCenterRightSignal()->value();
+}
+
 int get_create_vwall()
 {
 	return Create::instance()->virtualWall()->value();
