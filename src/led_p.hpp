bbb971a7ea86629fcc58f461f65ddc44b83e3921
Joshua Southerland <js@ou.edu>
2016-01-19 15:39:23 -0600

    LED implementation

---
+++ src/led_p.hpp
@@ -0,0 +1,21 @@
+/*
+ * led_p.hpp
+ *
+ *  Created on: Dec 30, 2015
+ *      Author: Joshua Southerland
+ */
+
+#ifndef SRC_LED_P_HPP_
+#define SRC_LED_P_HPP_
+
+namespace Private
+{
+
+void config_led();
+
+// TODO: move/remove
+void blink_led();
+
+}
+
+#endif /* SRC_LED_P_HPP_ */
