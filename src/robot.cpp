2c6729407cb73ce9dd920d62412d8991a3ff6eb8
Joshua Southerland <js@ou.edu>
2016-01-19 16:23:35 -0600

    Adding robot functions back so the API is not broken

---
+++ src/robot.cpp
@@ -0,0 +1,44 @@
+/*
+ * robot.cpp
+ *
+ *  Created on: Jan 2, 2016
+ *      Author: Joshua Southerland
+ */
+
+#include "wallaby/robot.hpp"
+#include "wallaby_p.hpp"
+
+unsigned long int Robot::getRobotStatesSequenceNumber()
+{
+	return 0;// TODO return Private::BattleHill::instance()->getRobotStatesSequenceNumber();
+}
+
+unsigned long int Robot::getRobotUpdateCount()
+{
+	return 0;// TODO return Private::BattleHill::instance()->getRobotUpdateCount();
+}
+
+unsigned short Robot::getRobotFirmwareVersion()
+{
+	return 0;// TODO return Private::BattleHill::instance()->getRobotFirmwareVersion();
+}
+
+bool Robot::setRobotUpdateDelay(unsigned int us_delay)
+{
+	return false;// TODO return Private::BattleHill::instance()->setRobotUpdateDelay(us_delay);
+}
+
+unsigned int Robot::getRobotUpdateDelay()
+{
+	return 0;// TODO return Private::BattleHill::instance()->getRobotUpdateDelay();
+}
+
+bool Robot::setLowVoltThreshold(float volts)
+{
+	return false;// TODO return Private::BattleHill::instance()->setLowVoltThreshold(volts);
+}
+
+float Robot::getLowVoltThreshold()
+{
+	return 0;// TODO return Private::BattleHill::instance()->getLowVoltThreshold();
+}
