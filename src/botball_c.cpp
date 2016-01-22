8df385e77a6134e7e00c54daab98fc0e05a4e840
Joshua Southerland <js@ou.edu>
2016-01-22 13:58:50 -0600

    Updating wait_for_light for our button and removing some graphics/display_printf usage

--- src/botball_c.cpp
+++ src/botball_c.cpp
@@ -4,6 +4,7 @@
 #include "wallaby/util.h"
 #include "wallaby/display.h"
 #include "wallaby/analog.h"
+#include "wallaby/digital.h"
 #include "wallaby/audio.h"
 #include "wallaby/graphics.h"
 #include "wallaby/thread.hpp"
@@ -66,79 +67,68 @@ VI void shut_down_in(double s)
 #define THRESHOLD 60    // minimum discrimination between light and dark
 #define RED 255,0,0
 #define GREEN 0,255,0
-void colorbar(int i);
-VI void wait_for_light(int light_port)
+void wait_for_light(int light_port)
 {
-	int xBut, OK=0, onval, offval, reading, cb=0, i; 
-	xBut=get_extra_buttons_visible(); // in case user has extra buttons visible
-	set_extra_buttons_visible(0);     //    turn them off
-	set_a_button_text("-");
-	set_c_button_text("-");
+  	int OK=0, onval, offval, reading;
+	const int button_port = 13;
+
+    set_digital_output(button_port, 0);
+
 	while (!OK) {
-		set_b_button_text("Light is ON");
-		display_clear();
-		display_printf (0,0,"CALIBRATE: sensor port #%d", light_port);
-		display_printf(0,1,"   press ON when light is on");
-		while(b_button()==0){
+		printf ("CALIBRATE: sensor port #%d\r\n", light_port);
+		printf("   press ON when light is on\r\n");
+		while(digital(button_port)==0){
 			msleep(100);
 			onval=analog(light_port); // sensor value when light is on
-			display_printf(0,2,"   light on value is = %d        ", onval);
+			printf("   light on value is = %d      \r\n", onval);
 		}
-		set_b_button_text("Light is OFF");
-		display_printf(0,1,"   light on value is = %d        ", onval);
+		printf("   light on value is = %d       \r\n", onval);
 		msleep(200);
-		beep();
-		while (b_button()); // debounce B button
-		display_printf(0,1,"   press OFF when light is off");
-		while (b_button()==0) {
+		while (digital(button_port)); // debounce B button
+		printf("   press OFF when light is off\r\n");
+		while (digital(button_port)==0) {
 			offval=analog(light_port);
-			display_printf(0,3,"   light off value is = %d         ", offval);
+			printf("   light off value is = %d         \r\n", offval);
 			msleep(100);
 		}
 		offval=analog(light_port); // sensor value when light is off
-		display_printf(0,3,"   light off value is = %d         ", offval);
+		printf("   light off value is = %d         \r\n", offval);
 		msleep(200);
 		if ((offval-onval)>=THRESHOLD) { // bright = small values
 			OK=1;
-			display_printf(0,4,"Good Calibration!");
-			display_printf(0,7,"Diff = %d:  WAITING FOR LIGHTS ON",offval-onval);
-			graphics_open(240,25);
-			graphics_fill(255,255,255);
-			graphics_update();
-			while (1) { // loop until light verified on
-				colorbar((i++)%74);
+			printf("Good Calibration!\r\n");
+			printf("Diff = %d:  WAITING FOR LIGHTS ON\r\n",offval-onval);
+            graphics_open(480,272);
+			graphics_fill(0,255,0);
+          	graphics_update();
+
+          	while (1) { // loop until light verified on
 				msleep(20);
 				reading=analog(light_port);
-				display_printf(0,8,"Current reading: %d ", reading);
+				printf("Current reading: %d \r\n", reading);
 				if ((reading-onval) < THRESHOLD) { // reading is low enough for light on
 					msleep(FLASHPAUSE); // pause
 					reading=analog(light_port);	// get second reading to rule out flash	
 				   if ((reading-onval) < THRESHOLD) break; // if still low enough, light is on
 				}
 			}
-			graphics_close();
-			display_printf(0,8,"Reading:%4d *** LIGHTS ARE ON ***", reading);
+			printf("Reading:%4d *** LIGHTS ARE ON ***\r\n", reading);
 		}
 		else {
-			display_printf(0,7,"BAD CALIBRATION");
+			printf("BAD CALIBRATION\r\n");
 			if (offval<256) {
-				display_printf(0,8,"   Add Shielding!!");
+				printf("   Add Shielding!!\r\n");
+                graphics_open(480,272);
+			    graphics_fill(255,0,0);
+          		graphics_update();
 				msleep(5000);
 			}
 			else {
-				display_printf(0,8,"   Aim sensor!!");
+				printf("   Aim sensor!!\r\n");
 				msleep(5000);
 			}
 		}
+
+        graphics_close();
 	}
-	set_a_button_text("A"); set_b_button_text("B"); set_c_button_text("C"); 
-	set_extra_buttons_visible(xBut); // in case user had extra buttons visible
-}
-void colorbar(int i) {
-	graphics_rectangle_fill(12,0,227,23,GREEN);
-	graphics_circle_fill(11,11,12,GREEN);
-	graphics_circle_fill(228,11,12,GREEN);
-	if (i<37) graphics_circle_fill(11+6*i,11,12,255,0,0);
-	else graphics_circle_fill(6+6*(74-i),11,12,255,0,0);
-	graphics_update();
 }
