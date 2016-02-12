6ccbd7faf3dc457ca56991a0c450085c5dcb62ce
Joshua Southerland <js@ou.edu>
2016-02-12 10:23:05 -0600

    New piecewise-linear battery apacity curve

--- src/battery_p.cpp
+++ src/battery_p.cpp
@@ -18,21 +18,30 @@ unsigned short battery_raw_reading(unsigned char * alt_read_buffer)
   return Private::Wallaby::instance()->readRegister16b(REG_RW_BATT_H, alt_read_buffer);
 }
 
+// linear interpolation
+float interp(float x, float xLo, float xHi, float yLo, float yHi)
+{
+	float yRange = yHi - yLo;
+	float xRange = xHi - xLo;
+	return (yRange * ((x - xLo) / xRange) + yLo);
+}
+
 float battery_power_level(unsigned short raw_batt)
 {
   // calculate voltage based on linear curve-fitting
-  double batt_voltage =  0.009175 * static_cast<double>(raw_batt);
+  double voltage =  0.009175 * static_cast<double>(raw_batt);
 
-  // convert to capacity
-  // TODO: better calibration
-  double empty_voltage = 5.5f;
-  double full_voltage = 6.6f;
-  double voltage_range = full_voltage - empty_voltage;
-  float perc = static_cast<float>((batt_voltage - empty_voltage) / voltage_range);
-  if (perc > 1.0f) perc = 1.0f;
-  if (perc < 0.0f) perc = 0.0f;
+  static const unsigned int NUM_SAMPLES = 5;
+  const float vs[NUM_SAMPLES] = {5.650, 6.413, 6.652, 6.707, 6.760}; // voltage
+  const float ps[NUM_SAMPLES] = {0.000, 0.065, 0.420, 0.760, 1.000}; // percentage / 100
 
-  return perc;
+  if (voltage < (vs[0] + 0.001)) return 0.0f;
+
+  for (unsigned int i = 0; i < NUM_SAMPLES-1; ++i)
+  {
+	  if (voltage < vs[i+1]) return interp(voltage, vs[i], vs[i+1], ps[i], ps[i+1]);
+  }
+  return 1.0f;
 }
 
 }
