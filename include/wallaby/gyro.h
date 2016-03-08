b1ff22e858cc82ba6b5d133b3f9382c3b2d80c20
Joshua Southerland <js@ou.edu>
2016-03-08 17:02:08 -0600

    More documentation

--- include/wallaby/gyro.h
+++ include/wallaby/gyro.h
@@ -13,12 +13,29 @@
 extern "C" {
 #endif
 
+/*!
+ * Gets the sensed x rotation
+ * \return The latest signed x rotation value
+ */
 signed short gyro_x();
 
+/*!
+ * Gets the sensed y rotation
+ * \return The latest signed y rotation value
+ */
 signed short gyro_y();
 
+/*!
+ * Gets the sensed z rotation
+ * \return The latest signed z rotation value
+ */
 signed short gyro_z();
 
+/*!
+ * Initiates a calibration of the gyrometer
+ * \note Not Yet Implemented
+ * \return 1: success 0: failure
+ */
 int gyro_calibrate();
 
 #ifdef __cplusplus
