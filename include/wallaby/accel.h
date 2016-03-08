5707388f17d5828562195529369d5478a8746f46
Joshua Southerland <js@ou.edu>
2016-03-08 16:23:28 -0600

    Adding documentation for several more headers

--- include/wallaby/accel.h
+++ include/wallaby/accel.h
@@ -12,12 +12,32 @@
 extern "C" {
 #endif
 
+/*!
+ * Gets the sensed x acceleration
+ * \return The latest signed x acceleration value
+ */
 signed short accel_x();
 
+
+/*!
+ * Gets the sensed x acceleration
+ * \return The latest signed y acceleration value
+ */
 signed short accel_y();
 
+
+/*!
+ * Gets the sensed x acceleration
+ * \return The latest signed z acceleration value
+ */
 signed short accel_z();
 
+
+/*!
+ * Initiates a calibration of the accelerometer
+ * \note Not Yet Implemented
+ * \return 1: success 0: failure
+ */
 int accel_calibrate();
 
 #ifdef __cplusplus
