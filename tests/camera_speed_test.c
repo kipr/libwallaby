0410ac82ecefa30f4be7427264d7b87c6e8eb22f
Joshua Southerland <js@ou.edu>
2016-11-15 22:49:26 +0000

    Adding camera speed test program

---
+++ tests/camera_speed_test.c
@@ -0,0 +1,38 @@
+#include <kipr/botball.h>
+
+int main()
+{
+    int ret;
+  
+    printf("Camera open\n");
+	camera_open_at_res(LOW_RES);
+    
+    printf("load config\n");
+    ret = camera_load_config("1");
+    if (ret == 1) printf("...success\n");
+  
+    camera_update();
+    printf("Cam width x height:  %d x %d\n", get_camera_width(), get_camera_height());
+  
+    printf("Num channels = %d\n", get_channel_count());
+  
+
+    int i;
+    double start = seconds();
+    const int num_imgs = 60;
+    for (i = 0; i < num_imgs; ++i)
+    {
+       camera_update();
+       int x = get_object_count(0) > 0 ? get_object_center_x(0,0) : 0;
+       int y = get_object_count(0) > 0 ? get_object_center_y(0,0) : 0;
+       // printf("%f objs: %d  (%d,%d)\n", seconds(), get_object_count(0), x, y); 
+     // printf("%f\n", seconds());
+    }
+    double stop = seconds();
+  
+    printf("%f fps\n", ((double)num_imgs/(stop-start)));  
+  
+    printf("Camera close\n");
+    camera_close();
+    return 0;
+}
