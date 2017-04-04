4c5c24b527cee1605fe9cd0a422fc9c80b6d07e9
Ryan Owens <ryanvader82@gmail.com>
2017-04-04 17:40:14 -0500

    started aruco stuff

---
+++ include/wallaby/aruco.h
@@ -0,0 +1,18 @@
+/*
+ * arhco.h
+ *
+ * Created on April 4, 2017
+ *      Author: Ryan Owens
+ */
+#ifndef INCLUDE_WALLABY_ARUCO_H_
+#define INCLUDE_WALLABY_ARUCO_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
