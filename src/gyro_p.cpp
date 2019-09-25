5d4c09aad7c45ccba947e4c9a3dcabae1dca8c7a
Zachary Sasser <ztsasser@gmail.com>
2019-09-25 11:18:45 -0500

    Update gyro_p.cpp

--- src/gyro_p.cpp
+++ src/gyro_p.cpp
@@ -8,29 +8,41 @@
 #include "gyro_p.hpp"
 #include "wallaby_p.hpp"
 #include "wallaby_regs_p.hpp"
-
+double bias = 0;
 namespace Private
 {
 
 short gyro_x(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_X_L, alt_read_buffer))/16;
+    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_X_L, alt_read_buffer))/16-bias;
 }
 
 short gyro_y(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Y_L, alt_read_buffer))/16;
+    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Y_L, alt_read_buffer))/16-bias;
 }
 
 short gyro_z(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Z_L, alt_read_buffer))/16;
+    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Z_L, alt_read_buffer))/16-bias;
 }
 
 bool gyro_calibrate()
 {
-  // TODO
-  return true;
+  ao();
+    msleep(300);
+    
+    //Find the average noise
+    int i = 0;
+    double avg = 0;
+    while(i<50){
+     	avg += gyro_z();
+        msleep(1);
+        i++;
+    }
+    bias = avg/50.0;
+
+    return 0;
 }
 
 bool gyro_calibrated(unsigned char * alt_read_buffer)
