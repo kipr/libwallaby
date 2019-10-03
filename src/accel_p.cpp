30b39089c9c0694bffdead781ff28988703706e3
Zachary Sasser <ztsasser@gmail.com>
2019-10-03 15:31:58 -0500

    Added Calibration for Accelerometer
    Probably unnecessary in most cases as the new IMU is pretty good.

--- src/accel_p.cpp
+++ src/accel_p.cpp
@@ -8,35 +8,61 @@
 #include "accel_p.hpp"
 #include "wallaby_p.hpp"
 #include "wallaby_regs_p.hpp"
+double biasx = 0, biasy = 0, biasz = 0;
 
 namespace Private
 {
 
 short accel_x(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_ACCEL_X_H, alt_read_buffer))/16;
+    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_ACCEL_X_H, alt_read_buffer))/16-biasx;
 }
 
 short accel_y(unsigned char * alt_read_buffer)
 {
-	return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_ACCEL_Y_H, alt_read_buffer))/16;
+	return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_ACCEL_Y_H, alt_read_buffer))/16-biasy;
 }
 
 short accel_z(unsigned char * alt_read_buffer)
 {
-	return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_ACCEL_Z_H, alt_read_buffer))/16;
+	return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_ACCEL_Z_H, alt_read_buffer))/16-biasz;
 }
 
-bool accel_calibrate()
+//Simple low-pass filter for accelerometer
+bool accel_calibrate(int samples = 50)
 {
-  // TODO
-  return true;
-}
+  ao();
+    msleep(300);
+    
+    //Find the average noise
+    int i = 0;
+    double avg = 0;
+    while(i<samples){
+     	avg += accel_z();
+        msleep(10);
+        i++;
+    }
+    biasz = avg/samples+512; //Z axis should be detecting gravity
+	
+    i = 0;
+    avg = 0;
+    while(i<samples){
+     	avg += accel_y();
+        msleep(10);
+        i++;
+    }
+    biasy = avg/samples;
+	
+	i = 0;
+    avg = 0;
+    while(i<samples){
+     	avg += accel_x();
+        msleep(10);
+        i++;
+    }
+    biasx = avg/samples;
 
-bool accel_calibrated(unsigned char * alt_read_buffer)
-{
-  // TODO
-  return true;
+    return 0;
 }
 
 }
