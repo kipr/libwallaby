63dabdc0689437477275da24e7762e5936f1e6ff
Prithviraj Kadiyala <pkadiyala@kipr.org>
2020-01-16 18:40:17 +0000

    Removing unwanted files

--- obj-arm-linux-gnueabihf/CMakeFiles/Docs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Docs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/Docs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
