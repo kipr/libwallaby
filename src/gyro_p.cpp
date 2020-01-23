a16a08809b69ed12ccfd49b5d338a3198d023afb
Prithviraj Kadiyala <pkadiyala@kipr.org>
2020-01-23 19:25:24 +0000

    Updating the Libwallaby to working files

--- src/gyro_p.cpp
+++ src/gyro_p.cpp
@@ -12,22 +12,23 @@
 #include "kipr/motors.h"
 
 double biasx = 0, biasy = 0, biasz = 0;
+unsigned char high, low;
 namespace Private
 {
 
 short gyro_x(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_X_L, alt_read_buffer))/16.375;
+    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_X_L, alt_read_buffer));
 }
 
 short gyro_y(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Y_L, alt_read_buffer))/16.375;
+    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Y_L, alt_read_buffer));
 }
 
 short gyro_z(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Z_L, alt_read_buffer))/16.375;
+    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Z_L, alt_read_buffer));
 }
 
 //Simple low-pass filter for gyroscope
