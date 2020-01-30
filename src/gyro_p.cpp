2c2159cb5ab0cad21f27ef27df3d0683754e021d
Zachary Sasser <ztsasser@gmail.com>
2020-01-30 13:39:09 -0600

    Fixed gyroscope to read high and low registers
    The data coming out of this input seems to still have an accuracy problem but it is decently precise. 
    You can probably use the gyro in this state to make a pretty good gyroscope library/function set but we need to figure out where that slight inaccuracy is coming from. 
    The output seems to have a spike every now and then but I can't find the cause just yet.

--- src/gyro_p.cpp
+++ src/gyro_p.cpp
@@ -1,8 +1,9 @@
 /*
  * gyro.cpp
  *
- *  Created on: Dec 31, 2015
- *      Author: Joshua Southerland
+ *  Created on: Dec 31, 2015 
+ *	Last Updated: January 21, 2020
+ *  Author(s): Joshua Southerland, Zachary Sasser
  */
 
 #include "gyro_p.hpp"
@@ -11,24 +12,35 @@
 #include "kipr/util.h"
 #include "kipr/motors.h"
 
+//Combines a high and low byte to make a 16 bit short integer.
+short combine(unsigned short low, unsigned short high){
+	return low + high*256;
+}
+
+//Biases default to zero so that the low pass only takes affect if calibration is run.
 double biasx = 0, biasy = 0, biasz = 0;
-unsigned char high, low;
+
 namespace Private
 {
-
+//Pulls gyroscope data from the I2C registers. The MPU 9250 outputs high and low registers that need to be combined.
 short gyro_x(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_X_L, alt_read_buffer));
+    return combine(static_cast<unsigned short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_X_L, alt_read_buffer)),
+					static_cast<unsigned short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_X_H, alt_read_buffer)) )-biasx;
 }
-
+	
+//Pulls gyroscope data from the I2C registers. The MPU 9250 outputs high and low registers that need to be combined.
 short gyro_y(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Y_L, alt_read_buffer));
+    return combine(static_cast<unsigned short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Y_L, alt_read_buffer)),
+					static_cast<unsigned short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Y_H, alt_read_buffer)) )-biasy;
 }
-
+	
+//Pulls gyroscope data from the I2C registers. The MPU 9250 outputs high and low registers that need to be combined.
 short gyro_z(unsigned char * alt_read_buffer)
 {
-    return static_cast<signed short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Z_L, alt_read_buffer));
+    return combine(static_cast<unsigned short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Z_L, alt_read_buffer)),
+					static_cast<unsigned short>(Private::Wallaby::instance()->readRegister16b(REG_RW_GYRO_Z_H, alt_read_buffer)) )-biasz;
 }
 
 //Simple low-pass filter for gyroscope
@@ -39,6 +51,8 @@ bool gyro_calibrate()
     msleep(300);
     
     //Find the average noise
+	
+    //Get the bias for the Z axis by sampling the stationary output.
     int i = 0;
     double avg = 0;
     while(i<samples){
@@ -48,7 +62,8 @@ bool gyro_calibrate()
     }
     biasz = avg/samples;
 	
-	i = 0;
+    //Get the bias for the Y axis by sampling the stationary output.
+    i = 0;
     avg = 0;
     while(i<samples){
      	avg += gyro_y();
@@ -57,7 +72,8 @@ bool gyro_calibrate()
     }
     biasy = avg/samples;
 	
-	i = 0;
+    //Get the bias for the X axis by sampling the stationary output.
+    i = 0;
     avg = 0;
     while(i<samples){
      	avg += gyro_x();
@@ -69,6 +85,7 @@ bool gyro_calibrate()
     return 0;
 }
 
+// This function is currently unused and meaningless...
 bool gyro_calibrated(unsigned char * alt_read_buffer)
 {
   // TODO
