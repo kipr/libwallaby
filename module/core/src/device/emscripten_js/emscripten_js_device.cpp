9c585869d1dd441c1adb8bb3a6c3755596da38a3
Braden McDorman <bmcdorman@gmail.com>
2022-08-22 22:15:46 -0700

    Use new SAB interface for emscripten_js

--- module/core/src/device/emscripten_js/emscripten_js_device.cpp
+++ module/core/src/device/emscripten_js/emscripten_js_device.cpp
@@ -1,36 +1,37 @@
 #include "../../device.hpp"
 #include "kipr/core/platform.hpp"
 
-
 #include "emscripten.h"
 
 namespace
 {
   const std::string NAME = "emscripten_js";
 
-  // emscripten function to write register values to JS registers variable
-  EM_JS(void, updateRegisters, (unsigned char *addresses, unsigned char *values, int length), {
-    const registers = [];
-    for (let regIndex = 0; regIndex < length; regIndex++) {
-      // Array values must be accessed directly from emscripten's memory buffer
-      const address = HEAPU8[addresses + regIndex];
-      const value = HEAPU8[values + regIndex];
-      if(Module.context.registers[address] !== value) {
-        Module.context.registers[address] = value;
-        registers.push({ address: address, value: value });
-      }
-    }
-
-    Module.context.onRegistersChange(registers);
+  // emscripten functions to write register values to JS
+  EM_JS(void, jsSetRegister8b, (unsigned char address, unsigned char value), {
+    Module.context.setRegister8b(address, value);
   });
 
-  // emscripten function to read register values from JS registers variable
-  EM_JS(unsigned char, readRegister, (unsigned char address), {
-    return Module.context.registers[address];
+  EM_JS(void, jsSetRegister16b, (unsigned char address, unsigned short value), {
+    Module.context.setRegister16b(address, value);
+  });
+
+  EM_JS(void, jsSetRegister32b, (unsigned char address, unsigned int value), {
+    Module.context.setRegister32b(address, value);
+  });
+
+  EM_JS(unsigned char, jsReadRegister8b, (unsigned char address), {
+    return Module.context.readRegister8b(address);
+  });
+
+  EM_JS(unsigned short, jsReadRegister16b, (unsigned char address), {
+    return Module.context.readRegister16b(address);
+  });
+
+  EM_JS(unsigned int, jsReadRegister32b, (unsigned char address), {
+    return Module.context.readRegister32b(address);
   });
 }
-
-
 
 class EmscriptenJsDevice : public kipr::core::Device
 {
@@ -46,76 +47,32 @@ public:
 
   virtual std::uint8_t r8(const std::uint8_t address) override
   {
-    emscripten_sleep(0);
-    return readRegister(address);
+    return jsReadRegister8b(address);
   }
 
   virtual std::uint16_t r16(const std::uint8_t address) override
   {
-    emscripten_sleep(0);
-
-    return (
-      readRegister(address) << 8 |
-      readRegister(address + 1)
-    );
+    return jsReadRegister16b(address);
   }
 
   virtual std::uint32_t r32(const std::uint8_t address) override
   {
-    emscripten_sleep(0);
-
-    return (
-      readRegister(address) << 24 |
-      readRegister(address + 1) << 16 |
-      readRegister(address + 2) << 8 |
-      readRegister(address + 3)
-    );
+    return jsReadRegister32b(address);
   }
 
   virtual void w8(const std::uint8_t address, const std::uint8_t value) override
   {
-    emscripten_sleep(0);
-    std::uint8_t addresses[] = { address };
-    std::uint8_t values[] = { value };
-    updateRegisters(addresses, values, 1);
+    jsSetRegister8b(address, value);
   }
 
   virtual void w16(const std::uint8_t address, const std::uint16_t value) override
   {
-    emscripten_sleep(0);
-    
-    std::uint8_t addresses[] = {
-      address,
-      static_cast<std::uint8_t>(address + 1)
-    };
-
-    std::uint8_t values[] = {
-      static_cast<std::uint8_t>((value & 0xFF00) >> 8),
-      static_cast<std::uint8_t>(value & 0x00FF)
-    };
-
-    updateRegisters(addresses, values, 2);
+    jsSetRegister16b(address, value);
   }
 
   virtual void w32(const std::uint8_t address, const std::uint32_t value) override
   {
-    emscripten_sleep(0);
-    
-    std::uint8_t addresses[] = {
-      address,
-      static_cast<std::uint8_t>(address + 1),
-      static_cast<std::uint8_t>(address + 2),
-      static_cast<std::uint8_t>(address + 3)
-    };
-
-    std::uint8_t values[] = {
-      static_cast<std::uint8_t>((value & 0xFF000000) >> 24),
-      static_cast<std::uint8_t>((value & 0x00FF0000) >> 16),
-      static_cast<std::uint8_t>((value & 0x0000FF00) >> 8),
-      static_cast<std::uint8_t>(value & 0x000000FF)
-    };
-    
-    updateRegisters(addresses, values, 4);
+    jsSetRegister32b(address, value);
   }
 };
 
